80cb07f041cd79b0fb6180bbf0c041bb57ae4c2c
Awes <28003@ma-web.nl>
2023-04-04 13:06:07 +0200

    Dashboard uitbreding, gegevens in gezet

--- js/main.js
+++ js/main.js
@@ -1,11 +1,44 @@
-const labels = ["Woonkamer", "Slaapkamer", "Speciale kamer", "Kelder", "Gym?"];
+const labels = [
+  "01/04/2023",
+  "01/04/2023",
+  "02/04/2023",
+  "02/04/2023",
+  "02/04/2023",
+  "02/04/2023",
+  "02/04/2023",
+  "02/04/2023",
+  "02/04/2023",
+  "02/04/2023",
+  "02/04/2023",
+  "02/04/2023",
+  "02/04/2023",
+  "02/04/2023",
+  "02/04/2023",
+  "02/04/2023",
+  "02/04/2023",
+  "02/04/2023",
+  "02/04/2023",
+  "02/04/2023",
+  "02/04/2023",
+];
 
 const data = {
   labels: labels,
 
   datasets: [
     {
-      label: "Verbruikte energie in KW",
+      label: "Energie verbruik April in kWh",
+      data: [1000, 1500, 2000, 2500, 3000],
+      backgroundColors: [
+        "#FF6B6B",
+        "#FFD93D",
+        "#6BCB77",
+        "#4D96FF",
+        "#F473B9,",
+      ],
+    },
+    {
+      label: "Gasverbruik April in m3",
       data: [1000, 1500, 2000, 2500, 3000],
       backgroundColors: [
         "#FF6B6B",
